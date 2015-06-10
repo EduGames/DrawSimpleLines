25f22b3852e0bc10bf99126ffcc939214f50f6ee
Mohammed Header <mohheader@hotmail.com>
2015-06-10 09:41:37 +0300

    Add LevelSelector Grid

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Level {
+	public Color bgColor;
+	public string Name;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/levels.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class levels : MonoBehaviour {
+	public Level[] levelsData;
+	public GameObject panel;
+	public GameObject btn;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Level level in levelsData) {
+			GameObject lvlBtn = Instantiate(btn);
+			lvlBtn.transform.SetParent(panel.transform);
+			lvlBtn.GetComponent<Image>().color = level.bgColor;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
